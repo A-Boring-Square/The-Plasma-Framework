33c5cfb5a4674a92dfa812fd3c5a600850c758e7
A-Boring-Square <123091707+A-Boring-Square@users.noreply.github.com>
2024-06-01 10:47:02 -0500

    Removed All Errors But Had To Nuke The Code

--- Plasma.cpp
+++ Plasma.cpp
@@ -1,1 +1,62 @@
 #include "Plasma.hpp"
+
+namespace Plasma {
+	namespace Concurrency {
+
+		template<typename ReturnType, typename... Args>
+		MutexedThreadManager<ReturnType, Args...>::MutexedThreadManager(std::function<ReturnType(Args...)> Func, Args&&... Args)
+			: Function(Func), ArgsTuple(std::forward<Args>(Args)...), ReturnValue(std::nullopt) {}
+
+		template<typename ReturnType, typename... Args>
+		void MutexedThreadManager<ReturnType, Args...>::StartThread() {
+			this->MutexedThread = std::thread([this]() {
+				std::lock_guard<std::mutex> lock(this->MutexForThread);
+				this->ReturnValue = std::apply(this->Function, this->ArgsTuple);
+				});
+		}
+
+		template<typename ReturnType, typename... Args>
+		void MutexedThreadManager<ReturnType, Args...>::JoinThread() {
+			if (this->MutexedThread.joinable()) {
+				this->MutexedThread.join();
+			}
+		}
+
+		template<typename ReturnType, typename... Args>
+		std::optional<ReturnType> MutexedThreadManager<ReturnType, Args...>::GetReturnValue() {
+			std::lock_guard<std::mutex> lock(this->MutexForThread);
+			return this->ReturnValue;
+		}
+
+		template<typename ReturnType, typename... Args>
+		MutexedThreadManager<ReturnType, Args...>::~MutexedThreadManager() {
+			if (this->MutexedThread.joinable()) {
+				this->MutexedThread.join();
+			}
+		}
+
+		template<typename ReturnType, typename ...Args>
+		ThreadManager<ReturnType, Args...>::ThreadManager(std::function<ReturnType(Args...)> Func, Args && ...Args)
+			: Function(Func), ArgsTuple(std::forward<Args>(Args)...), ReturnValue(std::nullopt) {
+
+		}
+
+		template<typename ReturnType, typename ...Args>
+		void ThreadManager<ReturnType, Args...>::StartThread() {
+			this->Thread = std::thread([this]() {
+				this->ReturnValue = std::apply(this->Function, this->ArgsTuple);
+				});
+
+		}
+
+		template<typename ReturnType, typename ...Args>
+		void Plasma::Concurrency::ThreadManager<ReturnType, Args...>::JoinThread() {
+			if (this->MutexedThread.joinable()) {
+				this->MutexedThread.join();
+			}
+		}
+
+
+
+	} // namespace Concurrency
+} // namespace Plasma
--- Plasma.hpp
+++ Plasma.hpp
@@ -1,2 +1,51 @@
 #pragma once
+#define WINDOWS_LEAN_AND_MEAN
+#include <Windows.h>
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <functional>
+#include <tuple>
+#include <optional>
 
+namespace Plasma {
+	namespace Concurrency {
+
+		template<typename ReturnType, typename... Args>
+		class MutexedThreadManager {
+		private:
+			std::thread MutexedThread;
+			std::mutex MutexForThread;
+			std::function<ReturnType(Args...)> Function;
+			std::tuple<Args...> ArgsTuple;
+			std::optional<ReturnType> ReturnValue;
+
+		public:
+			MutexedThreadManager(std::function<ReturnType(Args...)> Func, Args&&... Args);
+			void StartThread();
+			void JoinThread();
+			std::optional<ReturnType> GetReturnValue();
+			~MutexedThreadManager();
+		};
+
+		template<typename ReturnType, typename... Args>
+		class ThreadManager {
+		private:
+			std::thread Thread;
+			std::function<ReturnType(Args...)> Function;
+			std::tuple<Args...> ArgsTuple;
+			std::optional<ReturnType> ReturnValue;
+		public:
+			ThreadManager(std::function<ReturnType(Args...)> Func, Args&&... Args);
+			void StartThread();
+			void JoinThread();
+		};
+
+		
+	} // namespace Concurrency
+
+	namespace Ui {
+		// Future implementation for the Ui namespace
+	}
+
+} // namespace Plasma
